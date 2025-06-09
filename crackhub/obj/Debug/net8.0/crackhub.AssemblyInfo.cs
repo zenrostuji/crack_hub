5fda85f0159dc68a10bd24152af27c271194dda3
Trần Đức Mạnh <tranducmanh1412@gmail.com>
2025-06-09 12:48:24 +0700

    add khung

--- crackhub/obj/Debug/net8.0/crackhub.AssemblyInfo.cs
+++ crackhub/obj/Debug/net8.0/crackhub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("crackhub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5a1e8b32f83b4dcf2c30a9020fe023348fe5548")]
 [assembly: System.Reflection.AssemblyProductAttribute("crackhub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("crackhub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
